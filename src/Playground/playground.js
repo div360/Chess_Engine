c12507cec5bc5cc603c1b065357cb5c3006a1e49
Hrishabh <tiwarihd17@gmail.com>
2023-11-15 22:05:22 +0530

    playground added

---
+++ src/Playground/playground.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Board from '../Board/board'
+
+function Playground() {
+    return (
+        <div className='h-full w-full'>
+            <Board/>
+        </div>
+    )
+}
+
+export default Playground
