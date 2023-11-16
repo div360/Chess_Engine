c6e9b46ee53706d7ff17a9c09e14d1d0447d4fe5
Hrishabh <tiwarihd17@gmail.com>
2023-11-16 19:45:05 +0530

    playground added v2

--- src/Board/board.js
+++ src/Board/board.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { GiChessKing, GiChessQueen, GiChessBishop, GiChessKnight, GiChessRook, GiChessPawn } from "react-icons/gi";
+
 
 function Board () {
 
@@ -11,23 +13,26 @@ function Board () {
 
     return (
         <div className="flex justify-center items-center h-screen font-[Athiti] font-semibold bg-[#212121]">
-            <div className="grid grid-cols-8 w-[40%] h-[80%] p-4">
+            <div className="grid grid-cols-8 max-w-[40%] min-w-[40%] min-h-[80%] max-h-[80%]">
                 {board.map((row, rowIndex) =>
                     row.map((_, colIndex) => (
-
+                            <>
                         <div key={`${letters[colIndex]}${numbers[rowIndex]}`} 
-                            className={`${(rowIndex + colIndex) % 2 === 0 ? "bg-[#dddddd] text-green-800" : "bg-green-800 text-[#dddddd]"} 
+                            className={`flex flex-col items-center justify-center  w-24 h-24  ${(rowIndex + colIndex) % 2 === 0 ? "bg-[#c0c0c0] text-green-800" : "bg-green-800 text-[#dddddd]"} 
                             
                             ${colIndex === 0 && rowIndex !== 7 ? "pl-2 text-left" : ""} 
                             ${rowIndex === 7  && colIndex !== 0 ? "pr-2 text-right" : ""} 
                             ${rowIndex === 7 && colIndex === 0 ? "pl-2 text-left" : ""}
                             ` }
-                            
                         >
+                        {rowIndex === 6  && <GiChessPawn className="text-4xl text-center text-white" />}
+                        {rowIndex === 1  && <GiChessPawn className="text-4xl text-center text-black" />}
                            
-                            {`${colIndex === 0 ? numbers[rowIndex] : ""}${rowIndex === 7 ? letters[colIndex] : ""}`}
+                        {/* <div>{`${colIndex === 0 ? numbers[rowIndex] : ""}${rowIndex === 7 ? letters[colIndex] : ""}`}</div> */}
+                            
                         </div>
 
+                        </>
                     ))
                 )}
             </div>
