58a576f9e2f5e3a05f9ddba990ba291b65fd9cc0
Hrishabh <tiwarihd17@gmail.com>
2023-12-08 22:20:22 +0530

    add theme selection context

--- src/Button/themebutton.js
+++ src/Button/themebutton.js
@@ -1,8 +1,16 @@
 import { IoIosArrowRoundForward  } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
-export default function ThemeButton({text}) {
+export default function ThemeButton({redirect, text}) {
+
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+        navigate(redirect)
+    }
+
     return(
-        <div className='relative bg-black h-16 w-5/12'>
+        <div onClick={handleClick} className='relative bg-black h-16 w-5/12'>
             <button className='z-10 absolute right-[0.7vw] bottom-[1.1vh] w-full border-[0.2rem] px-2 py-2 border-black text-lg font-bold flex flex-row justify-center items-center gap-4 bg-white'>{text}<IoIosArrowRoundForward  size={40}/> </button>
         </div>
     )
--- src/Context/context.js
+++ src/Context/context.js
@@ -1,4 +1,4 @@
-
-import React, { createContext, useState } from 'react';
+import { createContext } from 'react';
 
 export const ChessContext = createContext();
+export const ChessUtilsContext = createContext();
